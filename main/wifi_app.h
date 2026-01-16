c1749e7c9302ad35664041e6edfe0192e25851af
Yevhen Bondarenko <evgenijbondarenko561@gmail.com>
2026-01-16 13:38:34 +0200

    Github_version (fake wifi data)

--- main/wifi_app.h
+++ main/wifi_app.h
@@ -5,8 +5,8 @@
 #include "freertos/event_groups.h"
 #include "esp_err.h"
 
-#define ESP_WIFI_SSID      "TP-Link_6780"
-#define ESP_WIFI_PASS      "78579170"
+#define ESP_WIFI_SSID      "Your_SSID"
+#define ESP_WIFI_PASS      "Your_PASSWORD"
 #define ESP_MAXIMUM_RETRY  5
 
 
